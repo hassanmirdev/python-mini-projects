86cbb0bfeee91f0c02b0c0f115af4e54b40ba0ea
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-10 21:26:45 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log(`Addition: ${add(5, 3)}`);
+console.log(`Subtraction: ${subtract(5, 3)}`);
+
+module.exports = { add, subtract };
